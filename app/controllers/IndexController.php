e9a19585db1f6d86f3f2a500bca99e21262d5088
Matthew Setter <matthew@maltblue.com>
2014-03-11 13:34:49 +0100

    Added in the core index and pages controllers with views templates

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * The Default controller
+ */
+class IndexController extends \Phalcon\Mvc\Controller
+{
+    /**
+     * Default controller
+     */
+    public function indexAction()
+    {
+        // home
+    }
+}
